3609521a4a1c0f8bfd0e9ce073927939b487ce1e
Mariovega99 <mariovegarcia.1999@gmail.com>
2021-02-20 02:56:47 +0100

    Actualiza el despliegue en Heroku

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^wdpiaqqmzv+)ufqoksd&yspd$+#+*6&hwujj-ho33et&p_rpl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
