a67da4bf677bc8de6e4af0451d26c09f69196453
Mariovega99 <mariovegarcia.1999@gmail.com>
2021-02-20 02:38:45 +0100

    Commit inicial

---
+++ posts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+from posts.models import Post # new
+
+admin.site.register(Post)     # new
+
---
+++ posts/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import ListView # new
+from .models import Post                  # new
+
+
+class HomePageView(ListView):             # new
+    model = Post                          # new
+    template_name = 'home.html'           # new
+    context_object_name = 'all_posts_list'          # new
